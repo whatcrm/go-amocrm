8763731582ed56f754f47abd8829d575958cc1f6
lanyere <agzam.erlan@gmail.com>
2023-12-28 12:21:17 +0600

    fix: company CustomFieldsValues

--- models/companies.go
+++ models/companies.go
@@ -1,17 +1,17 @@
 package models
 
 type Company struct {
-	ID                int             `json:"id,omitempty"`
-	Name              string          `json:"name,omitempty"`
-	ResponsibleUserID int             `json:"responsible_user_id,omitempty"`
-	CreatedBy         int             `json:"created_by,omitempty"`
-	UpdatedBy         int             `json:"updated_by,omitempty"`
-	CreatedAt         int             `json:"created_at,omitempty"`
-	UpdatedAt         int             `json:"updated_at,omitempty"`
-	CustomFieldsValue []CustomFields  `json:"custom_fields_value,omitempty"`
-	CompanyEmbedded   CompanyEmbedded `json:"_embedded,omitempty"`
-	RequestID         string          `json:"request_id,omitempty"`
-	Links             struct {
+	ID                 int             `json:"id,omitempty"`
+	Name               string          `json:"name,omitempty"`
+	ResponsibleUserID  int             `json:"responsible_user_id,omitempty"`
+	CreatedBy          int             `json:"created_by,omitempty"`
+	UpdatedBy          int             `json:"updated_by,omitempty"`
+	CreatedAt          int             `json:"created_at,omitempty"`
+	UpdatedAt          int             `json:"updated_at,omitempty"`
+	CustomFieldsValues []CustomFields  `json:"custom_fields_values,omitempty"`
+	CompanyEmbedded    CompanyEmbedded `json:"_embedded,omitempty"`
+	RequestID          string          `json:"request_id,omitempty"`
+	Links              struct {
 		Self struct {
 			Href string `json:"href,omitempty"`
 		} `json:"self,omitempty"`
