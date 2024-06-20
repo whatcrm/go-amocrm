337cc6cac8500e933c95aba50b7fbd34a3d8c80b
chausov_i <prog14@zabtoniryi.ru>
2024-06-20 10:50:32 +0300

    test

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/whatcrm/go-amocrm"
+
+const (
+	clientID     = "Your-Client-ID"
+	clientSecret = "Your-Client-Secret"
+	redirectURI  = "Your-Redirect-URL"
+)
+
+func main() {
+
+	println("test")
+
+	domain := "example.amocrm.ru"
+	accessToken := "Your-Access-Token"
+
+	amoCRM := amocrm.NewAPI(clientID, clientSecret, redirectURI)
+	amoCRM.SetOptions(domain, accessToken, true)
+
+	/*
+
+		Now, you can use the *amoCRM* (*API) to make requests to amoCRM/Kommo
+		In order for it to work, you have to maintain tokens,
+		therefore make sure to save them and retrieve when needed.
+
+		You can update your tokens with TokenUPD method, like in examples/tokens.go
+
+	*/
+}
