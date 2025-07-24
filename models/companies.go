9622da0737721468cb8fdd0b9098dfd54e80573f
lanyere <agzam.erlan@gmail.com>
2025-07-24 11:15:03 +0200

    fix: anonymous structs, to prevent marshaling unexpected fields [Issue: https://github.com/whatcrm/go-amocrm/issues/6]

--- models/companies.go
+++ models/companies.go
@@ -1,22 +1,17 @@
 package models
 
 type Company struct {
-	ID                 int             `json:"id,omitempty"`
-	Name               string          `json:"name,omitempty"`
-	ResponsibleUserID  int             `json:"responsible_user_id,omitempty"`
-	CreatedBy          int             `json:"created_by,omitempty"`
-	UpdatedBy          int             `json:"updated_by,omitempty"`
-	CreatedAt          int             `json:"created_at,omitempty"`
-	UpdatedAt          int             `json:"updated_at,omitempty"`
-	CustomFieldsValues []CustomFields  `json:"custom_fields_values,omitempty"`
-	CompanyEmbedded    CompanyEmbedded `json:"_embedded,omitempty"`
-	RequestID          string          `json:"request_id,omitempty"`
-	Links              struct {
-		ID   int `json:"id,omitempty"`
-		Self struct {
-			Href string `json:"href,omitempty"`
-		} `json:"self,omitempty"`
-	} `json:"_links,omitempty"`
+	ID                 int              `json:"id,omitempty"`
+	Name               string           `json:"name,omitempty"`
+	ResponsibleUserID  int              `json:"responsible_user_id,omitempty"`
+	CreatedBy          int              `json:"created_by,omitempty"`
+	UpdatedBy          int              `json:"updated_by,omitempty"`
+	CreatedAt          int              `json:"created_at,omitempty"`
+	UpdatedAt          int              `json:"updated_at,omitempty"`
+	CustomFieldsValues []CustomFields   `json:"custom_fields_values,omitempty"`
+	CompanyEmbedded    *CompanyEmbedded `json:"_embedded,omitempty"`
+	RequestID          string           `json:"request_id,omitempty"`
+	Links              *LinkResponse    `json:"_links,omitempty"`
 }
 
 type CompanyEmbedded struct {
